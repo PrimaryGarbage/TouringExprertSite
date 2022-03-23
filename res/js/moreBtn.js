9cc6b396e19e81cbc4ab6e1d6f8396bbf74c7de2
prim <artstudioexp@gmail.com>
2022-03-23 23:12:55 +0300

    Add additional info block to aboutus section

---
+++ res/js/moreBtn.js
@@ -0,0 +1,41 @@
+const moreBtns = document.getElementsByClassName("more-btn");
+const defaultHeights = [];
+const hideTimeout = 1000;
+
+for(b of moreBtns)
+{
+    b.addEventListener("click", openMore);
+}
+
+function openMore(event)
+{
+    event.target.style.transform = "rotateX(180deg)"; 
+    event.target.parentElement.style.height = "fit-content";
+    const oldHeight = event.target.parentElement.offsetHeight;
+    defaultHeights[event.target] = oldHeight + "px";
+    const additionalInfo = event.target.parentElement.getElementsByClassName("additional-info")[0];
+    additionalInfo.style.display = "block";
+    event.target.parentElement.style.height = oldHeight + "px";
+    event.target.parentElement.style.height = (oldHeight + additionalInfo.offsetHeight) + "px";
+    event.target.removeEventListener("click", openMore);
+    event.target.addEventListener("click", closeMore);
+}
+
+function closeMore(event)
+{
+    event.target.style.transform = "rotateX(0deg)";
+    event.target.parentElement.style.height = defaultHeights[event.target];
+    setTimeout(hideMore, hideTimeout, event);
+    event.target.removeEventListener("click", closeMore);
+    event.target.addEventListener("click", openMore);
+}
+
+function hideMore(event)
+{
+    event.target.parentElement.getElementsByClassName("additional-info")[0].style.display = "none";
+}
+
+function showMore(event)
+{
+    const additionalInfo = event.target.parentElement.getElementsByClassName("additional-info")[0].style.display = "block"
+}
