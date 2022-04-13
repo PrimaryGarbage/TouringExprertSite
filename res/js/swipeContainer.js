afff4dd8f6afe3e9d16b77687fac7cc68e28b88a
prim <artstudioexp@gmail.com>
2022-04-13 16:00:48 +0300

    Add working OneStripeContainer

--- res/js/swipeContainer.js
+++ res/js/swipeContainer.js
@@ -10,8 +10,23 @@ class Container {
         this.stripElements = stripElements;
         this.prevBtn = prevBtn;
         this.nextBtn = nextBtn;
-        this.positionX = 0;
+        this.positionX = 0;     // in percent
         this.dotsContainerElement = dotsContainerElement;
+    }
+}
+
+class OneStripContainer {
+    constructor(containerElement, stripElement, prevBtn, nextBtn, dotsContainerElement, gap, elementWidth, maxPosX)
+    {
+        this.containerElement = containerElement;
+        this.stripElement = stripElement;
+        this.prevBtn = prevBtn;
+        this.nextBtn = nextBtn;
+        this.dotsContainerElement = dotsContainerElement;
+        this.gap = gap;
+        this.elementWidth = elementWidth;
+        this.maxPosX = maxPosX;
+        this.positionX = 0;     // in pixels
     }
 }
 
@@ -21,62 +36,128 @@ function init()
 {
     for(c of containerElements)
     {
-        let containerChildren = liveToStaticCollection(c.children);
-        containerChildren.pop();
-        const stripLength = window.innerWidth > widthThreshold? Number(c.getAttribute("strip-length")) : Number(c.getAttribute("strip-length-narrow"));
-        const numberOfStrips = Math.ceil(containerChildren.length / stripLength);
-        const navBtns = c.getElementsByClassName("nav-btns")[0];
-        for(let i = 0; i < numberOfStrips; ++i)
-        {
-            let strip = document.createElement("div");
-            strip.classList += "strip-swipe-subcontainer";
-            strip.style.gridTemplateColumns = `repeat(${stripLength}, 1fr)`;
-            for(let j = 0; j < stripLength && i * stripLength + j < containerChildren.length; ++j)
-            {
-                const idx = i * stripLength + j;
-                strip.appendChild(containerChildren[idx]);
-            }
-            c.insertBefore(strip, navBtns);
-        }
-
-        const strips = c.getElementsByClassName("strip-swipe-subcontainer");
-
-        for(s of strips)
-        {
-            const eventBoxes = s.getElementsByClassName("event-box");
-            for(e of eventBoxes)
-            {
-                e.addEventListener("click", gotoEventURL);
-            }
-        }
-        
-        const prevBtn = c.getElementsByClassName("prev-event-btn")[0];
-        const nextBtn = c.getElementsByClassName("next-event-btn")[0];
-        prevBtn.addEventListener("click", prev);
-        nextBtn.addEventListener("click", next);
-        if(c.getAttribute("nav-buttons") == "false")
-        {
-            prevBtn.parentElement.style.display = "none";
-        }
-    
-        let dotsContainer = document.createElement("div");
-        for(let i = 0; i < strips.length; ++i)
-        {
-            let dot = document.createElement("span");
-            dot.classList += "event-dot";
-            dot.setAttribute("index", i);
-            dot.addEventListener("click", dotClicked);
-            dotsContainer.appendChild(dot);
-        }
-        dotsContainer.children[0].classList = "event-dot-active";
-        c.insertBefore(dotsContainer, prevBtn.parentElement);
-        if(c.getAttribute("dots") == "false")
-        {
-            dotsContainer.style.display = "none";
-        }
-
-        containers.push(new Container(c, strips, prevBtn, nextBtn, dotsContainer));
+        if(c.getAttribute("one-strip") == "true") initOneStripContainer(c);
+        else initContainer(c);
     }
+}
+
+function initContainer(container)
+{
+    let containerChildren = liveToStaticCollection(container.children);
+    containerChildren.pop();
+    const stripLength = window.innerWidth > widthThreshold? Number(container.getAttribute("strip-length")) : Number(container.getAttribute("strip-length-narrow"));
+    const numberOfStrips = Math.ceil(containerChildren.length / stripLength);
+    const navBtns = container.getElementsByClassName("nav-btns")[0];
+    for(let i = 0; i < numberOfStrips; ++i)
+    {
+        let strip = document.createElement("div");
+        strip.classList += "strip-swipe-subcontainer";
+        strip.style.gridTemplateColumns = `repeat(${stripLength}, 1fr)`;
+        for(let j = 0; j < stripLength && i * stripLength + j < containerChildren.length; ++j)
+        {
+            const idx = i * stripLength + j;
+            strip.appendChild(containerChildren[idx]);
+        }
+        container.insertBefore(strip, navBtns);
+    }
+
+    const strips = container.getElementsByClassName("strip-swipe-subcontainer");
+
+    // MOVE THIS SHITe!!!
+    for(s of strips)
+    {
+        const eventBoxes = s.getElementsByClassName("event-box");
+        for(e of eventBoxes)
+        {
+            e.addEventListener("click", gotoEventURL);
+        }
+    }
+    
+    const prevBtn = container.getElementsByClassName("prev-event-btn")[0];
+    const nextBtn = container.getElementsByClassName("next-event-btn")[0];
+    prevBtn.addEventListener("click", prev);
+    nextBtn.addEventListener("click", next);
+    if(c.getAttribute("nav-buttons") == "false")
+    {
+        prevBtn.parentElement.style.display = "none";
+    }
+
+    let dotsContainer = document.createElement("div");
+    for(let i = 0; i < strips.length; ++i)
+    {
+        let dot = document.createElement("span");
+        dot.classList += "event-dot";
+        dot.setAttribute("index", i);
+        dot.addEventListener("click", dotClicked);
+        dotsContainer.appendChild(dot);
+    }
+    dotsContainer.children[0].classList = "event-dot-active";
+    c.insertBefore(dotsContainer, prevBtn.parentElement);
+    if(c.getAttribute("dots") == "false")
+    {
+        dotsContainer.style.display = "none";
+    }
+
+    containers.push(new Container(container, strips, prevBtn, nextBtn, dotsContainer));
+}
+
+function initOneStripContainer(container)
+{
+    let containerChildren = liveToStaticCollection(container.children);
+    containerChildren.pop();
+    const stripLength = window.innerWidth > widthThreshold? Number(container.getAttribute("strip-length")) : Number(container.getAttribute("strip-length-narrow"));
+    let numberOfElements = containerChildren.length;
+    const navBtns = container.getElementsByClassName("nav-btns")[0];
+    let strip = document.createElement("div");
+    strip.classList += "strip-swipe-subcontainer";
+    strip.style.gridTemplateColumns = `repeat(${numberOfElements}, 1fr)`;
+    for(let i = 0; i < numberOfElements; ++i)
+    {
+        strip.appendChild(containerChildren[i]);
+    }
+    container.insertBefore(strip, navBtns);
+
+    const prevBtn = container.getElementsByClassName("prev-event-btn")[0];
+    const nextBtn = container.getElementsByClassName("next-event-btn")[0];
+    prevBtn.addEventListener("click", prevOneStrip);
+    nextBtn.addEventListener("click", nextOneStrip);
+    if(c.getAttribute("nav-buttons") == "false")
+    {
+        prevBtn.parentElement.style.display = "none";
+    }
+
+    let dotsContainer = document.createElement("div");
+    for(let i = 0; i < numberOfElements - stripLength + 1; ++i)
+    {
+        let dot = document.createElement("span");
+        dot.classList += "event-dot";
+        dot.setAttribute("index", i);
+        dot.addEventListener("click", dotClickedOneStrip);
+        dotsContainer.appendChild(dot);
+    }
+    dotsContainer.children[0].classList = "event-dot-active";
+    c.insertBefore(dotsContainer, prevBtn.parentElement);
+    if(c.getAttribute("dots") == "false")
+    {
+        dotsContainer.style.display = "none";
+    }
+
+    let elementWidth = containerChildren[0].offsetWidth;
+    let gap = (strip.offsetWidth - elementWidth * stripLength) / (stripLength - 1);
+    gapVW = (pxtovw(gap));
+    strip.style.gap = gapVW + "vw";
+
+    const maxPosX = - (containerChildren.length - stripLength) * (gap + elementWidth);
+
+    const newContainer = new OneStripContainer(container, strip, prevBtn, nextBtn, dotsContainer, gap, elementWidth, maxPosX)
+
+    const animTime = container.getAttribute("animation-time");
+    if(animTime)
+    {
+        animateOneStripContainer(newContainer, animTime);
+    }
+
+    containers.push(newContainer);
 }
 
 function next(event)
@@ -155,6 +236,7 @@ function dotClicked(event)
     goto(Number(event.currentTarget.getAttribute("index")), container);
 }
 
+
 function goto(index, container)
 {
     const newPos = -clamp(100 * index, 0, 100 * (container.stripElements.length - 1));
@@ -166,17 +248,116 @@ function goto(index, container)
 
     if(container.containerElement.getAttribute("dots") == "true")
     {
-        for(dot of container.dotsContainerElement.childNodes)
+        for(dot of container.dotsContainerElement.children)
         {
             dot.classList = "event-dot";
         }
-        container.dotsContainerElement.childNodes[Math.abs(container.positionX / 100.0)].classList = "event-dot-active";
+        container.dotsContainerElement.childNodes[index].classList = "event-dot-active";
+    }
+}
+
+function nextOneStrip(event)
+{
+    let container;
+    for(c of containers)
+    {
+        if(c.containerElement.isEqualNode(event.currentTarget.parentElement.parentElement))
+        {
+            container = c;
+            break;
+        }
+    }
+
+    if(Math.round(container.positionX) > container.maxPosX)
+    {
+        const newPos = container.positionX - container.elementWidth - container.gap;
+        container.stripElement.style.transform = `translateX(${newPos}px)`;
+        container.positionX = newPos;
+        
+        if(container.containerElement.getAttribute("dots") == "true")
+        {
+            let activeIdx;
+            for(let i = 0; i < container.dotsContainerElement.children.length; ++i)
+            {
+                if(container.dotsContainerElement.children[i].classList == "event-dot-active") activeIdx = i;
+                container.dotsContainerElement.children[i].classList = "event-dot";
+            }
+            container.dotsContainerElement.children[activeIdx+1].classList = "event-dot-active";
+        }
+    }
+    else
+    {
+        gotoOneStrip(0, container);
+    }
+}
+
+function prevOneStrip(event)
+{
+    let container;
+    for(c of containers)
+    {
+        if(c.containerElement.isEqualNode(event.currentTarget.parentElement.parentElement))
+        {
+            container = c;
+            break;
+        }
+    }
+
+    if(Math.round(container.positionX) < 0)
+    {
+        const newPos = container.positionX + container.elementWidth + container.gap;
+        container.stripElement.style.transform = `translateX(${newPos}px)`;
+        container.positionX = newPos;
+        
+        if(container.containerElement.getAttribute("dots") == "true")
+        {
+            let activeIdx;
+            for(let i = 0; i < container.dotsContainerElement.children.length; ++i)
+            {
+                if(container.dotsContainerElement.children[i].classList == "event-dot-active") activeIdx = i;
+                container.dotsContainerElement.children[i].classList = "event-dot";
+            }
+            container.dotsContainerElement.children[activeIdx-1].classList = "event-dot-active";
+        }
+    }
+    else
+    {
+        gotoOneStrip(container.stripElement.children.length - container.containerElement.getAttribute("strip-length"), container);
+    }
+}
+
+function dotClickedOneStrip(event)
+{
+    let container;
+    for(c of containers)
+    {
+        if(c.containerElement.isEqualNode(event.currentTarget.parentElement.parentElement))
+        {
+            container = c;
+            break;
+        }
+    }
+    gotoOneStrip(Number(event.currentTarget.getAttribute("index")), container);
+}
+
+function gotoOneStrip(index, container)
+{
+    const newPos = - index * (container.gap + container.elementWidth);
+    container.stripElement.style.transform = `translateX(${newPos}px)`;
+    container.positionX = newPos;
+    if(container.containerElement.getAttribute("dots") == "true")
+    {
+        for(dot of container.dotsContainerElement.children)
+        {
+            dot.classList = "event-dot";
+        }
+        container.dotsContainerElement.children[index].classList = "event-dot-active";
     }
 }
 
 function gotoEventURL(event)
 {
-    const href = event.currentTarget.getAttribute("href")
+    const href = event.currentTarget.getAttribute("href");
     if(href) window.location.href = href;
 }
 
@@ -193,4 +374,17 @@ function liveToStaticCollection(liveCollection)
         staticCollection.push(element);
     }
     return staticCollection;
+}
+
+function pxtovw(px)
+{
+    return px / window.innerWidth * 100;
+}
+
+function animateOneStripContainer(container, switchTime)
+{
+    setInterval(() => { 
+        const event = new Event("click");
+        container.nextBtn.dispatchEvent(event);
+    }, switchTime)
 }
